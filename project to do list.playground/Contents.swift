31ed03fa95600e5934b95b37e328f911fc433abd
Mohammed Mabruk <Mohammed.Mabruk.med.s@tuwaiq.edu.sa>
2021-10-13 15:07:24 +0300

    new updat

--- project to do list.playground/Contents.swift
+++ project to do list.playground/Contents.swift
@@ -1,7 +1,13 @@
 import UIKit
 
 
-enum TaskStatus {
+enum TaskStatus:Int,Comparable
+{
+    static func < (lhs: TaskStatus, rhs: TaskStatus) -> Bool {
+        return TaskStatus.completed > TaskStatus.pending
+        
+    }
+    
     case completed
     case pending
 }
@@ -39,9 +45,9 @@ class ToDo {
     func readAll(){
         for list in lists {
             print("List Title * \(list.listTitle) \(list.items.count))")
-//            for item in list.items {
-//                print("------- Item \(item.taskTitle), status: \(item.taskStatus)")
-//            }
+           for item in list.items {
+               print("------- Item \(item.taskTitle), status: \(item.taskStatus)")
+           }
         }
         }
    
@@ -55,19 +61,20 @@ var task1 = Item.init(taskTitle: "shopping", taskStatus:.completed)
 var task2 = Item.init(taskTitle: "cleen myroom", taskStatus:.pending)
 var task3 = Item.init(taskTitle: "meeting", taskStatus: .completed)
 var task4 = Item.init(taskTitle: "visit my uncle", taskStatus: .completed)
-
 var myLists = ToDo(list: List(listTitle: "my work list", items:[task1 , task2, task3]))
 // Create New List
 myLists.lists.append(List(listTitle: "tuaiwq list", items: [Item(taskTitle: "Finish Project", taskStatus: .pending)]))
 // Create an item for 1st List
+
+
+
 myLists.lists[0].addTask(item: task4)
-//myLists.lists[0].deleteAtask(index: 0)
+myLists.lists[0].deleteAtask(index: 0)
 // Update Item in List, Change Title
 myLists.lists[0].updateAtask(index: 0, task: "Buy water")
 // Start Nested For Loop in all lists and print items in each list
+
 myLists.readAll()
-
-
 
                
 
